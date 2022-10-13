c4b373a3cb4695773a9206a9c185243e6473efde
AlinaVedernikova <113028651+AlinaVedernikova@users.noreply.github.com>
2022-10-13 13:55:28 +0300

    Update Theory.cs

--- Theory.cs
+++ Theory.cs
@@ -1,125 +1,217 @@
-﻿using System;
+using System;
 
-namespace _3rd_Lab
+namespace lab2
 {
-    class Theory
+    class Program
     {
         static void Main(string[] args)
         {
-            #region Collections Theory
-            /* Array - static (restricted) massive of data in the memory. If we want to extend it, we should create a new array and copy existing data there.
-             * access to each number of array have static time that don't depend on size of array. So get arr[10] and arr[25426] consume equal time.
-             * use it when you have determinative amount of elements.
-             * 
-             *
-             *
-             * These more suitable collections available if you add System.Collections.Generic library
-             *
-             *
-             * Stack<type of data> - pile of elements (like a books on the table or plates in the sink) 
-             * that lay one over another and you can add new one to the top, look at the uppest one or take it
-             * 
-             * Queue<type of data> - like in the shop or on escalator in the subway. You can add to the end, look the first element or take the first element.
-             * 
-             * ArrayList - is a non-generic collection of objects whose size increases dynamically. It is the same as Array except that its size increases dynamically.
-             * It is a common array for different types. You can keep there int, double, string etc in one array.
-             *
-             * List<type of data> - a collection of strongly typed objects that can be accessed by index and having methods for sorting, searching, and modifying list. 
-             * It is the generic version of the ArrayList/ Use it when you don't know the limit of your array and ought to add or remove elements sometimes (linked array)
-             * the PC create an array and when you want to add new element, it create a new array and copy all existing elements there.
-             *
-             * LinkedList<type of data> - array with a reference to the next and previous element (twice-linked array)
-             * 
-             * Dictionary<type of key, type of data> - pair key-value. You can add new key, find key, get value by key, set value by key. 
-             * Very fast access (as simple array), but more flexible
-             * 
-             */
+            #region level 1 number 8
+            Console.WriteLine("--------level 1 number 8--------");
+            double x,y;
+            Console.WriteLine("Enter x:");
+            double.TryParse(Console.ReadLine(), out x);
+            if (Math.Abs(x) >= 1) y = 0;
+            else y = x * x - 1;
+            Console.WriteLine("y = {0}", y);
             #endregion
 
-            #region Arrays
+            #region level 1 number 2
+            Console.WriteLine("--------level 1 number 2--------");
+            Console.WriteLine("Enter (x;y):");
+            double.TryParse(Console.ReadLine(),out x);
+            double.TryParse(Console.ReadLine(), out y);
+            if (y >= 0 && y + Math.Abs(x) <= 1) Console.WriteLine("dot in triangle");
+            else Console.WriteLine("dot not in triangle");
+            #endregion
 
-            //Dimensions:
-            int[] oneDimension = new int[1000000]; // row with a 1 million if zeros
-            double[] oneDimensionInitializedArray; // undefined array
-            int[,] twoDimension = new int[1000, 1000]; // matrix 1000 x 1000, filled by zeros
-            string[,,] threeDimension = new string[5, 10, 255]; 
-            // 5 rows, 10 columns and 255 elements in the column. Each element can contain a string (so it 4-th dimension array actually)
+            #region level 2 number 1
+            Console.WriteLine("--------level 2 number 1--------");
+            int n;
+            double height, summary=0;
+            Console.WriteLine("Enter n: ");
+            int.TryParse(Console.ReadLine(), out n);
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Enter {0} student's height:", i);
+                double.TryParse(Console.ReadLine(),out height);
+                if(height>0) summary += height;
+                else
+                {
+                    do
+                    {
+                        Console.WriteLine("Enter positive height");
+                        double.TryParse(Console.ReadLine(), out height);
+                    } while (height <= 0);
+                }
+            }
+            Console.WriteLine("average height of students: {0}", summary/n);
+            #endregion
 
-            int[][] notSquarMatrix = new int[15][]; // array where each element contain an array (different or equal lengths) -> [ [0,1,2,3,4] , [10,25] , [8] ];
+            #region level 2 number 9
+            Console.WriteLine("--------level 2 number 9--------");
+            Console.WriteLine("Enter n");
+            int.TryParse(Console.ReadLine(), out n);
+            double maxi=0.0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Enter {0} result: ", i);
+                double.TryParse(Console.ReadLine(), out x);
+                if (x > maxi) maxi = x;
 
-            //and so on
-
-            // Access to element:
-
-            threeDimension[threeDimension.Length-1, 0, threeDimension.GetLength(2)-1] = "I am the latest element here!"; 
-            // do not forget that start with 0 and end with Length-1!
-
-
-            double[] shortExample = new double[4] { 1.2, 0.154564, -454, 0 }; // will be crated a new massive with 4 elements
-
-            // Use arrays for limited length. Otherwise - List.
-            // For fast search - Dictionary
-            // Efficient simple solutions usually realizing by Stack and Queue
-            // LinkedList is used seldom
+            }
+            Console.WriteLine("Best result: {0}", maxi);
 
             #endregion
-                
-            #region Tuples
 
-            //It is linked links to several variables in the fixed order:
-            (string name, int age, double height) student = ("Vasiliy", 20, 1.89);
-            (int[] marks, int average) table = ({1,2,3,4,5}, 3);.
-                
-            string Name = student.name; // or student.Item1
+            #region level 3 number 4
+            Console.WriteLine("--------level 3 number 4--------");
+            double r1, r2;
+            int Res = 0;
+            Console.WriteLine("Enter r1 and r2: ");
+            double.TryParse(Console.ReadLine(), out r1);
+            double.TryParse(Console.ReadLine(), out r2);
+            while (r1 <= 0 && r2 <= r1) 
+            {
+
+                Console.WriteLine("r1 must be > 0 and < r2, try again");
+                double.TryParse(Console.ReadLine(), out r1);
+                double.TryParse(Console.ReadLine(), out r2);
+            } 
             
-            var tuple = (count:5, sum:10);
-            Console.WriteLine(tuple.count); // 5
-            Console.WriteLine(tuple.sum); // 10
-            
-            // It is the simpliest structure of data (not an array!)
-            #endregion
            
-            #region Enum
-
-            //It is order of integers (Int 8 / 16 / 32 / 64) where you create a list of names and each name get the value (increment by 1 of previous):              
-            enum Marks
+            Console.WriteLine("Enter x and y, or x > 1000 to exit");
+            do
             {
-                Bad = 2,
-                Nice, // auto-incremented to 3
-                Good, // 4 ...
-                Excellent // not coma at the end!
-            }
-            
-            enum Classes
-            {
-                Math = 1,
-                PhysicalCulture = 3, // Jump over
-                History = 4,
-                Dinner = 2 // Instead Informatics
-            }
-            
-            int myAvgMark = (int)Marks.Bad + (int)Marks.Excellent; // cast to int!
-            Console.WriteLine((Classes)10); // expancion to enum values
-            
-            public enum Season
-            {
-                Spring,
-                Summer,
-                Autumn,
-                Winter
-            }
-            
-            Season a = Season.Autumn;
-            Console.WriteLine($"Integral value of {a} is {(int)a}");  // output: Integral value of Autumn is 2
+                Console.WriteLine("x and y:");
+                double.TryParse(Console.ReadLine(), out x);
+                if (x >= 1000) break;
+                double.TryParse(Console.ReadLine(), out y);
+                if (x * x + y * y <= r2 * r2 && x * x + y * y >= r1*r1)
+                {
+                    Res += 1;
+                }
+            } while (x < 1000);
+            Console.WriteLine("dots in the ring: {0}", Res);
 
-            var b = (Season)1;
-            Console.WriteLine(b);  // output: Summer
 
-            var c = (Season)4;
-            Console.WriteLine(c);  // output: 4
-            
-            // More informative than variables, more flexible than constans, more strict than string, more effective than dictionary :) cool thing!
-            #endregion       
+            #endregion
+
+            #region level 3 number 11
+            Console.WriteLine("--------level 3 number 11--------");
+            //n студентов, каждый 4 экзамена, число неуспевающих и средний балл группы - ?
+            int exam1 = 2, exam2 = 2, exam3 = 2, exam4 = 2, res = 0, amount = 0, bad_st = 0;
+
+            do
+            {
+                Console.WriteLine("Enter exam 1, for exit - 6");
+                int.TryParse(Console.ReadLine(), out exam1);
+                if (exam1 == 6) break;
+                Console.WriteLine("Enter exam 2");
+                int.TryParse(Console.ReadLine(), out exam2);
+                Console.WriteLine("Enter exam 3");
+                int.TryParse(Console.ReadLine(), out exam3);
+                Console.WriteLine("Enter exam 4");
+                int.TryParse(Console.ReadLine(), out exam4);
+                while (exam1 < 2 || exam2 < 2 || exam2 > 5 || exam3 < 2 || exam3 > 5 || exam4 < 2 || exam4 > 5)
+                {
+                    Console.WriteLine("Enter positive results");
+                    int.TryParse(Console.ReadLine(), out exam1);
+                    if (exam1 == 6) break;
+                    Console.WriteLine("Enter exam 2");
+                    int.TryParse(Console.ReadLine(), out exam2);
+                    Console.WriteLine("Enter exam 3");
+                    int.TryParse(Console.ReadLine(), out exam3);
+                    Console.WriteLine("Enter exam 4");
+                    int.TryParse(Console.ReadLine(), out exam4);
+                }
+                if (exam1 != 2 && exam2 != 2 && exam3 != 2 && exam4 != 2)
+                {
+                    amount += 4;
+                    res += (exam4 + exam3 + exam2 + exam1);
+                }
+                else bad_st++;
+            } while (exam1 < 6);
+            Console.WriteLine("Amount of bad students: {0}, average score: {1}", bad_st, res / (0.0 + (amount)));
+
+            #endregion
+
+            #region level 3 number 12
+            Console.WriteLine("--------level 3 number 12--------");
+            double r = 0;
+            int S;
+            do
+            {
+                Console.WriteLine("Enter r, for exit - 100");
+                double.TryParse(Console.ReadLine(), out r);
+                if (r == 100) break;
+                Console.WriteLine("Enter 1 for S of circle, 2 for S of square, 3 for S of triangle");
+                int.TryParse(Console.ReadLine(), out S);
+                switch (S)
+                {
+                    case 1:
+                        Console.WriteLine("S of circle with radius {0}: {1}", r, Math.PI * r * r);
+                        break;
+                    case 2:
+                        Console.WriteLine("S of square with side {0}: {1}", r,  r * r);
+                        break;
+                    case 3:
+                        Console.WriteLine("S of triangle with side {0}: {1}", r, r * r * Math.Sqrt(3) / 4);
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+
+            } while (r < 100 && r>0);
+            #endregion
+
+            #region level 3 number 13
+            Console.WriteLine("--------level 3 number 13--------");
+            double A = 0, B;
+            do
+            {
+                Console.WriteLine("Enter A, for exit enter 100");
+                double.TryParse(Console.ReadLine(), out A);
+                while (A <= 0)
+                {
+                    Console.WriteLine("Enter positive A");
+                    double.TryParse(Console.ReadLine(), out A);
+                }
+                if (A == 100) break;
+                Console.WriteLine("Enter B");
+                double.TryParse(Console.ReadLine(), out B);
+                while (B <= 0)
+                {
+                    Console.WriteLine("Enter positive B");
+                    double.TryParse(Console.ReadLine(), out B);
+                }
+                Console.WriteLine("Enter 1 for S of rectangle, 2 for S of ring, 3 for S of triangle");
+                int.TryParse(Console.ReadLine(), out S);
+                switch (S)
+                {
+                    case 1:
+                        Console.WriteLine("S of rectangle with sides {0} and {1}: {2}", A, B, A * B);
+                        break;
+                    case 2:
+                        Console.WriteLine("S of ring between circles with radiuses {0} and {1}: {2}", A, B, Math.Abs(Math.PI*A*A-Math.PI*B*B));
+                        break;
+                    case 3:
+                        while (B * B - (A * A / 4) < 0)
+                        {
+                            Console.WriteLine("Enter another B and A (triangle with this sides is imposible");
+                            double.TryParse(Console.ReadLine(), out A);
+                            double.TryParse(Console.ReadLine(), out B);
+                        }
+                            
+                        Console.WriteLine("S of triangle with sides {0} and {1}: {2}", A, B, 1/2*A*Math.Sqrt(B*B-(A*A/4.0)));
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+            } while (A < 100);
+            #endregion
         }
     }
 }
