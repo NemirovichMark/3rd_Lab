575f1a780f4b617b501ea056f024474701d14194
Golokova Sofya <113624984+khobaess@users.noreply.github.com>
2022-11-01 20:03:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace алгоритмы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
