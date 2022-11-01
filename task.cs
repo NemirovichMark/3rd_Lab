aaabbcdc4cafbc03432c89d6bf75fd57453f4937
Oleg Chavykin <92402494+OlegChavykin@users.noreply.github.com>
2022-11-01 03:50:05 +0300

    Update task.cs

--- task.cs
+++ task.cs
@@ -1,4 +1,5 @@
 
+
 #region lvl1Task6
 double[] array = new double[5];
 Console.WriteLine("Enter array values");
@@ -18,20 +19,20 @@ double Q = Convert.ToDouble(Console.ReadLine());
 double P = Convert.ToDouble(Console.ReadLine());
 if (P > Q)
 {
-    Console.WriteLine("Enter array values:");
-    for (int i = 0; i < 10; i++)
-    {
-        array[i] = Convert.ToDouble(Console.ReadLine());
-        if (Q < array[i] && array[i] < P)
-        {
-            count++;
-        }
-    }
-    Console.WriteLine($"{count} elements of array stand between P and Q");
+Console.WriteLine("Enter array values:");
+for (int i = 0; i < 10; i++)
+{
+array[i] = Convert.ToDouble(Console.ReadLine());
+if (Q < array[i] && array[i] < P)
+{
+count++;
+}
+}
+Console.WriteLine($"{count} elements of array stand between P and Q");
 }
 else
 {
-    Console.WriteLine("Error");
+Console.WriteLine("Error");
 }
 #endregion
 
@@ -41,17 +42,17 @@ int count = 0;
 Console.WriteLine("Enter array values");
 for (int i = 0; i < 10; i++)
 {
-    array[i] = Convert.ToDouble(Console.ReadLine());
-    if (array[i] > 0) count++;
+array[i] = Convert.ToDouble(Console.ReadLine());
+if (array[i] > 0) count++;
 }
 double[] array_positive = new double[count];
 for (int i = 0; i < 10; i++)
 {
-    if (array[i] > 0)
-    {
-        array_positive[count - 1] = array[i];
-        count--;
-    }
+if (array[i] > 0)
+{
+array_positive[count - 1] = array[i];
+count--;
+}
 }
 Console.WriteLine(string.Join("", array_positive));
 #endregion
@@ -63,20 +64,20 @@ double negative_index = 0;
 Console.WriteLine("Enter array value");
 for (int i = 0; i < 8; i++)
 {
-    array[i] = Convert.ToDouble(Console.ReadLine());
-    if (array[i] < 0)
-    {
-        negative_value = array[i];
-        negative_index = i;
-    }
+array[i] = Convert.ToDouble(Console.ReadLine());
+if (array[i] < 0)
+{
+negative_value = array[i];
+negative_index = i;
+}
 }
 if (negative_value == 0)
 {
-    Console.Write("You don't enter negative value");
+Console.Write("You don't enter negative value");
 }
 else
 {
-    Console.WriteLine($"Value - {negative_value}\nNubmer - {negative_index + 1}");
+Console.WriteLine($"Value - {negative_value}\nNubmer - {negative_index + 1}");
 }
 #endregion
 
@@ -89,20 +90,20 @@ int index2 = 0;
 Console.WriteLine("Enter array values");
 for (int i = 0; i < 10; i++)
 {
-    array[i] = Convert.ToDouble(Console.ReadLine());
+array[i] = Convert.ToDouble(Console.ReadLine());
 }
 for (int i = 0; i < 10; i++)
 {
-    if (i % 2 == 0)
-    {
-        array_1[index1] = array[i];
-        index1++;
-    }
-    if (i % 2 != 0)
-    {
-        array_2[index2] = array[i];
-        index2++;
-    }
+if (i % 2 == 0)
+{
+array_1[index1] = array[i];
+index1++;
+}
+if (i % 2 != 0)
+{
+array_2[index2] = array[i];
+index2++;
+}
 }
 Console.WriteLine($"1st array - {String.Join("", array_1)}");
 Console.WriteLine($"2nd array - {String.Join("", array_2)}");
@@ -513,34 +514,170 @@ Console.WriteLine(string.Join(" ", array2));
 #region 11
 static int BinarySearch(double[] array, double x)
 {
-    int i1 = 0;
-    int i2 = array.Length-1;
-    int i = (i1+i2) / 2;
-    while (i2 > i1)
-    {
-        if (array[i] == x)
-        {
-            return i;
-        }
-        else
-        {
-            if (array[i] > x)
-            {
-                i2 = i + 1;
-                i = (i1+i2) / 2;
-            }
-            else
-            {
-                i1 = i + 1;
-                i = (i1 + i2) / 2;
-            }
-        }
-    }
-    return -1;
+int i1 = 0;
+int i2 = array.Length-1;
+int i = (i1+i2) / 2;
+while (i2 > i1)
+{
+if (array[i] == x)
+{
+return i;
 }
-//Exemple
+else
+{
+if (array[i] > x)
+{
+i2 = i + 1;
+i = (i1+i2) / 2;
+}
+else
+{
+i1 = i + 1;
+i = (i1 + i2) / 2;
+}
+}
+}
+return -1;
+}
+//Пример
 double[] array = new double[] { 1, 2, 3, 4, 5 };
 double x = 11;
 Console.WriteLine(BinarySearch(array,x));
+
+#endregion
+
+#region 12
+static int[] Task12(int[] a, int[] b)
+{
+int[] c = new int[a.Length + b.Length];
+
+int min = Math.Min(a.Length, b.Length);
+for (int i = 0; i < min; i++)
+{
+c[2 * i] = a[i];
+c[2*i+1] = b[i];
+}
+if (a.Length != b.Length)
+{
+int count = min * 2;
+if (a.Length > b.Length)
+{
+for (int i = min; i < a.Length; i++)
+{
+c[count] = a[i];
+count++;
+}
+}
+else
+{
+for (int i = min; i < b.Length; i++)
+{
+c[count] = b[i];
+count++;
+}
+}
+}
+return c;
+}
+//Пример
+int[] a = new int[] { 1, 3, 5, 7};
+int[] b = new int[] { 0, 2, 4, 6,8,10,12};
+Console.WriteLine(string.Join(" ",Task12(a, b)));
+#endregion
+
+#region 13
+static int[] Task13(int[] a, int[] b)
+{
+int temp = 0;
+for (int i = 0; i < a.Length; i++)
+{
+for (int j = i + 1; j < a.Length; j++)
+{
+if (a[i] < a[j])
+{
+a[i] = a[j];
+a[j] = temp;
+}
+}
+}
+temp = 0;
+for (int i = 0; i < b.Length; i++)
+{
+for (int j = i + 1; j < b.Length; j++)
+{
+if (b[i] < b[j])
+{
+b[i] = b[j];
+b[j] = temp;
+}
+}
+}
+
+int index_a = 0;
+int index_b = 0;
+int[] c = new int[a.Length + b.Length];
+
+for (int i = 0; i < c.Length; i++)
+{
+if (index_a == a.Length)
+{
+c[i] = b[index_b];
+index_b++;
+continue;
+}
+if (index_b == b.Length)
+{
+c[i] = a[index_a];
+index_a++;
+continue;
+}
+if (a[index_a] > b[index_b])
+{
+c[i] = a[index_a];
+index_a++;
+}
+else
+{
+c[i] = b[index_b];
+index_b++;
+}
+}
+return c;
+}
+#endregion
+
+#region 14
+static int[] Reverse(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
+    }
+    return array;
+}
+#endregion
+
+#region 15
+static int[] Sdvig(int[] array)
+{
+    int[] result = new int[array.Length];
+    int count = 0;
+    int f= Convert.ToInt32(Console.ReadLine());
+    for (int i = array.Length - (f % array.Length); i < array.Length; i++)
+    {
+        result[count] = array[i];
+        count++;
+    }
+
+    for (int i = 0; i < array.Length - (f % array.Length); i++)
+    {
+        result[count] = array[i];
+        count++;
+    }
+    return result;
+}
+
 
 #endregion
