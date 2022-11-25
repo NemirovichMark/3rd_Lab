4c1265f4dc6b8effa80b9402bce09ade8a25fd39
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-11-25 11:58:50 +0300

    Create Level2_Task5.cs

---
+++ Level2_Task5.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        try{
+            Console.Write("Enter an array in a row: ");
+            var a = Console.ReadLine().Split(' ').Select(n => double.Parse(n)).ToArray();
+            double[] b = new double[0];
+            int mx=0,mn=0;
+            for(int i=1; i<a.Length; i++){
+                if(a[i]>a[mx]) mx=i;
+                if(a[i]<a[mn]) mn=i;
+            }
+            Console.WriteLine($"Minimal element: {a[mn]}\nMaximal element: {a[mx]}");
+            if(mn>mx) (mn,mx)=(mx,mn);
+            for(int i=mn+1; i<mx;i++){
+                if(a[i]<0){
+                    Array.Resize(ref b,b.Length+1);
+                    b[b.Length-1]=a[i];
+                }
+            }
+            Console.Write("This is an answer array: ");
+            foreach(var i in b)
+                Console.Write("{0}",i);
+        }
+        catch{
+            Console.WriteLine("Wrong input error");
+        }
+        
+    }
+}
