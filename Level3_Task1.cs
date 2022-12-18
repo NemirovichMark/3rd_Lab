7dcee0b01c2b4eb0f76801297fdbcc4a5640b6ee
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-18 22:51:20 +0300

    Update Level3_Task1.cs

--- Level3_Task1.cs
+++ Level3_Task1.cs
@@ -6,18 +6,20 @@ public class HelloWorld
     public static void Main(string[] args)
     {
         try{
-            Console.Write("Enter the array in a row: ");
+            Console.Write("Write the array in a row: ");
             double[] a=Console.ReadLine().Split().Select(n => double.Parse(n)).ToArray();
-            int[] b = new int[a.Length];
-            int index=0;
-            for(int i=1; i<a.Length; i++)
-                if(a[i]>a[index]) index=i;
-            int c=0;
-            for(int i=0; i<a.Length; i++)
-                if(a[i]==a[index])
-                    b[c++]=i;
-            Array.Resize(ref b, c);
-            Console.Write("This is the answer array:");
+            int[] b = new int[1]{0};
+            for(int i=1; i<a.Length; i++){
+                if(a[i]==a[b[0]]){
+                    Array.Resize(ref b, b.Length+1);
+                    b[b.Length-1]=i;
+                }
+                else if(a[i]>a[b[0]]){
+                    Array.Resize(ref b, 1);
+                    b[b.Length-1]=i;
+                }
+            }
+            Console.Write($"Max element: {a[b[0]]}\nThis is the array made of indexes of the max elements:");
             foreach(var i in b)
                 Console.Write($" {i}");
         }
