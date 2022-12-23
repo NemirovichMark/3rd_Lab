cd4cc885e02eeafb9bcfe6ec6465cb3760d6f710
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-23 09:38:20 +0300

    Create Level1_Task11.cs

---
+++ Level1_Task11.cs
@@ -0,0 +1,28 @@
+using System;
+class HelloWorld
+{
+  static void Main ()
+  {
+    int size = 10;
+    int counter = 0; 
+    double[] x = new double[size];
+    double[] y = new double[counter];
+    for (int i = 0; i < size; i++){
+  Console.Write($"Enter the {i+1} element of the array: ");
+  if (!double.TryParse (Console.ReadLine (), out x[i])) return;
+        if (x[i] > 0 ){
+            Array.Resize(ref y,++counter);
+            y[counter-1]=x[i];
+        }
+    }
+    Console.Write("Your array: ");
+    foreach(int element in x){
+        Console.Write($"{element}");
+    }
+    Console.Write("\n");
+    foreach(int element in y){
+        Console.Write($"{element} ");
+    }
+    
+  }
+}
