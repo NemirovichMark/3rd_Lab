d68824dd7de4b60a32700d270f227f272f437e32
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-23 10:23:22 +0300

    Update Level3_Task12.cs

--- Level3_Task12.cs
+++ Level3_Task12.cs
@@ -6,13 +6,14 @@ public class HelloWorld
     public static void Main(string[] args)
     {
         try{
-            Console.Write("Write the array in a row: ");
+            Console.Write("Write the array in a row (size 12): ");
             double[] a=Console.ReadLine().Split().Select(t => double.Parse(t)).ToArray();
+            //if(a.Length!=12) return;
             int m=0;
             for(int i=0; i<a.Length; i++)
                 if(a[i]>=0)
                     a[m++]=a[i];
-            Array.Resize(ref a, a.Length-m);
+            Array.Resize(ref a, m);
             Console.Write("The answer array:");
             foreach(var i in a)
                 Console.Write($" {i}");
