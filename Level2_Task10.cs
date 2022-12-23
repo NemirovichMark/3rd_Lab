ddb86d0c68f0e885c5c2abf69484fbea0cfa0404
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-23 10:36:28 +0300

    Update Level2_Task10.cs

--- Level2_Task10.cs
+++ Level2_Task10.cs
@@ -9,11 +9,17 @@ public class HelloWorld
             Console.Write("Enter an array in a row: ");
             var a = Console.ReadLine().Split(' ').Select(n => double.Parse(n)).ToArray();
             double[] b = new double[a.Length-1];
-            int mn=0;
-            for(int i=1; i<a.Length; i++){
-                if(a[i]<a[mn]) mn=i;
+            int mn=-1;
+            for(int i=0; i<a.Length&&a[i]<=0;i++) mn=i;
+            mn++;
+            if(mn==a.Length){
+                Console.Write("There are no positive elements");
+                return;
             }
-            Console.WriteLine($"Minimal element: {mn}");
+            for(int i=mn+1; i<a.Length; i++){
+                if(a[i]<a[mn] && a[i]>0) mn=i;
+            }
+            Console.WriteLine($"Minimal element: a[{mn}]");
             int c=0;
             for(int i=0; i<a.Length;i++){
                 if(i==mn)
@@ -22,7 +28,7 @@ public class HelloWorld
             }
             Console.WriteLine("This is the answer array: ");
             foreach(var i in b)
-                Console.Write("{0} ",i);
+                Console.Write(" {0}",i);
         }
         catch{
             Console.WriteLine("Wrong input error");
