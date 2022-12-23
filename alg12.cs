6bcbfbeb3072b3c60e998fd1fc45b88253b5f6a3
EmilTsvetanov0 <113028536+EmilTsvetanov0@users.noreply.github.com>
2022-12-24 01:08:10 +0300

    Update alg12.cs

--- alg12.cs
+++ alg12.cs
@@ -8,13 +8,20 @@ class HelloWorld {
             double[] a=Console.ReadLine().Split().Select(t => double.Parse(t)).ToArray();
             Console.Write("Write the second array: ");
             double[] b=Console.ReadLine().Split().Select(t => double.Parse(t)).ToArray();
-            if(a.Length<b.Length) Array.Resize(ref b,a.Length);
-            else Array.Resize(ref a,b.Length);
-            double[] c = new double[a.Length*2];
-            for(int i=0; i<a.Length; i++){
+            int len=a.Length;
+            if(a.Length>b.Length) len=b.Length;
+            double[] c = new double[a.Length+b.Length];
+            for(int i=0; i<len; i++){
                 c[i*2]=a[i];
                 c[i*2+1]=b[i];
             }
+            int counter=len*2;
+            if(len==a.Length)
+                for(int i=len; i<b.Length; i++)
+                    c[counter++]=b[i];
+            else
+                for(int i=len; i<a.Length; i++)
+                    c[counter++]=a[i];
             Console.Write("Answer array:");
             foreach(var i in c) Console.Write($" {i}");
         }
